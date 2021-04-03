5dcfaaa2dd641ddc467e0e24ee879e368464b453
Marigi <marigindegwa@gmail.com>
2021-04-03 05:46:01 +0300

    created a sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 
 end

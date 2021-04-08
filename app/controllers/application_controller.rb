cffdb90b86c356d341483915095e024da3cda65f
Marigi <marigindegwa@gmail.com>
2021-04-08 05:18:44 +0300

    Edit password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ before_action :set_current_user
       Current.user = User.find_by(id: session[:user_id])
     end    
   end
+
+  def require_user_logged_in!
+    redirect_to sign_in_path, alert: "You must be signed in to do that." if Current.user.nil?
+  end
 end

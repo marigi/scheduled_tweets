57d58640f4a20c937576cf9a561ad1bb2448c225
Marigi <marigindegwa@gmail.com>
2021-04-28 02:50:52 +0300

    Tweet Partial

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module ScheduledTweets
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    #config.time_zone = 'Nairobi (Africa/Nairobi)'
+    config.time_zone = "Nairobi"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
